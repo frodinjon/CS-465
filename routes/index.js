4a8a5360cbff213b168480ef0969e62520dab6f3
frodinjon <jon.frodin@snhu.edu>
2021-07-02 13:56:17 -0700

    Express webiste, static HTML only

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+//const ctrlMain = require('../controllers/main');
+
+/* GET home page. */
+//router.get('/', ctrlMain.index);
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
